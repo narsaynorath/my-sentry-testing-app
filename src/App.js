8739b0fc8a810f5a65b3c958f470bd49efc75954
Nar Saynorath <nar@narsaynorath.com>
2021-10-26 19:21:18 -0400

    Okay I added a new file and commit now

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 
+import BrokenButton from './BrokenButton';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <BrokenButton />
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
