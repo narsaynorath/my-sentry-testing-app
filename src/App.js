5b7934bbe0fda3410aa2b9664d30e952e0375f4d
Nar Saynorath <nar@narsaynorath.com>
2021-10-26 15:35:30 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ function App() {
         >
           Learn React
         </a>
+        <button onClick={iDontExist}>I don't exist</button>
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
+
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+
+Sentry.init({
+  dsn: "https://25ad4c6524574802a12b5d3e4758cc6b@o1052590.ingest.sentry.io/6036357",
+  integrations: [new Integrations.BrowserTracing()],
+  tracesSampleRate: 1.0,
+})
+
 
 ReactDOM.render(
   <React.StrictMode>
@@ -14,4 +24,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
